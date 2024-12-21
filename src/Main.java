85df7215965eee120d7cc86666a6efb389482617
VISVANATH123 <viswa3643@gmail.com>
2024-12-21 16:45:05 +0530

    Initial commit java print

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int  a = scan.nextInt();
+        if (a > 20) {
+            System.out.println("greater.");
+        } else {
+            System.out.println("Lesser.");
+        }
+
+
+        Main siva = new Main();
+        System.out.println(siva.visva());
+    }
+
+    public int visva() {
+        System.out.println("visvanath r");
+        return 0;
+
+    }
+}
